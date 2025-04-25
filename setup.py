707646edb8bd55c69117a2adde162ab8231d660f
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-04-25 19:13:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="advanced-ocr",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit==1.12.0",
+        "pytesseract==0.3.10",
+        "easyocr==1.6.2",
+        "paddleocr==2.6.1.3",
+        "pdf2image==1.16.3",
+        "opencv-python-headless==4.6.0.66",
+        "pillow==9.5.0",
+        "python-docx==0.8.11",
+        "openpyxl==3.1.2",
+        "pandas==1.5.3",
+        "numpy==1.24.3",
+    ],
+)
